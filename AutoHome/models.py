70ba572d14ef418fff4fc965d03fe25b7bb2023a
owlhoo <anes1996_h@hotmail.com>
2019-03-16 00:32:48 +0100

    out of the db world :3

--- AutoHome/models.py
+++ AutoHome/models.py
@@ -10,21 +10,30 @@ from django.contrib.auth.models import User
 
 
 class Timer(models.Model):
-    from_time = models.DateTimeField(null=True)
-    to_time = models.DateTimeField(null=True)
+    _from_time = models.DateTimeField(verbose_name='Scheduled from time', null=True)
+    _to_time = models.DateTimeField(verbose_name='Scheduled to time', null=True)
+
+    @property
+    def to_time(self):
+        return self._from_time
+
+    @to_time.setter
+    def to_time(self, new_to_time):
+        self._to_time = new_to_time
+
+    @property
+    def from_time(self):
+        return self._from_time
+
+    @from_time.setter
+    def from_time(self, new_from_time):
+        self._from_time = new_from_time
 
     def calculate_time(self):
-        return self.to_time - self.from_time
-
-    def set_time(self, from_time, to_time):
-        self.from_time = from_time
-        self.to_time = to_time
-
-    def get_time(self):
-        return  str(self.from_time) + ' - ' + str(self.to_time)
+        return self._to_time - self._from_time
 
     def __str__(self):
-        return self.get_time()
+        return 'Scheduled from:' + str(self.from_time) + ' to ' + str(self.to_time)
 
 
 class Room(models.Model):
@@ -43,92 +52,135 @@ class Room(models.Model):
 
 
 class Device(models.Model):
-    modified = models.DateTimeField()
-    dev_room = models.ForeignKey(Room, on_delete=models.CASCADE)
-    dev_power = models.BooleanField('Device power')
-    dev_timer = models.OneToOneField(Timer, on_delete=models.CASCADE)
-    dev_name = models.CharField(verbose_name='Name of device', max_length=256)
-
-    @property
-    def modification_time(self):
-        return self.modified
-
-    @modification_time.setter
-    def modification_time(self, value):
-        self.modified = value
-
-    @property
-    def room(self):
-        return self.dev_room.name
-
-    @room.setter
-    def room(self, name):
-        self.dev_room.name = name
-
-    @property
-    def power(self):
-        return self.dev_power
-
-    @power.setter
-    def power(self, new_power):
-        self.dev_power = new_power
+    _dev_timer = models.OneToOneField(Timer, verbose_name='Device timer', null=True, on_delete=models.CASCADE)
+    _dev_room = models.ForeignKey(Room, verbose_name='Device room', null=True, on_delete=models.CASCADE)
+    _dev_name = models.CharField(verbose_name='Name of device', max_length=256)
+    _modified = models.DateTimeField(verbose_name='Time modified')
+    _dev_power = models.BooleanField(verbose_name='Device power')
 
     @property
     def timer(self):
-        return self.dev_timer.get_time()
+        return self._dev_timer
 
     @timer.setter
-    def timer(self, time):
-        self.dev_timer.set_time(time[0], time[1])
+    def timer(self, new_timer):
+        self._dev_timer = new_timer
+
+    @property
+    def room(self):
+        return self._dev_room
+
+    @room.setter
+    def room(self, new_room):
+        self._dev_room = new_room
 
     @property
     def name(self):
-        return self.dev_name
+        return self._dev_name
 
     @name.setter
     def name(self, new_name):
-        self.dev_name = new_name
+        self._dev_name = new_name
+
+    @property
+    def modification_time(self):
+        return self._modified
+
+    @modification_time.setter
+    def modification_time(self, value):
+        self._modified = value
+
+    @property
+    def power(self):
+        return self._dev_power
+
+    @power.setter
+    def power(self, new_power):
+        self._dev_power = new_power
 
     def __str__(self):
-        return self.dev_name + ' in the ' + self.dev_room.name
-
-
-# TODO test profile
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    last_accessed = models.DateTimeField(null=True)
-    device = models.ManyToManyField(Device)
-
-    def __str__(self):
-        return self.user.username
-
-# TODO test sensor
+        return self._dev_name + ' in the ' + self._dev_room.name
 
 
 class Sensor(models.Model):
-    value = models.FloatField(default=0)
-    name = models.CharField(max_length=256)
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    _critical_temperature = models.FloatField(verbose_name='Critical temperature', default=0)
+    _room = models.ForeignKey(Room, verbose_name='Sensor timer', on_delete=models.CASCADE)
+    _temperature = models.FloatField(verbose_name='Sensor temperature', default=0)
+    _humidity = models.FloatField(verbose_name='Sensor humidity', default=0)
+    _name = models.CharField(verbose_name='Sensor name', max_length=256)
+
+    @property
+    def critical_temp(self):
+        return self._critical_temperature
+
+    @critical_temp.setter
+    def critical_temp(self, value):
+        self._critical_temperature = value
+
+    @property
+    def temperature(self):
+        return self._temperature
+
+    @temperature.setter
+    def temperature(self, new_temperature):
+        self._temperature = new_temperature
+
+    @property
+    def humidity(self):
+        return self._humidity
+
+    @humidity.setter
+    def humidity(self, new_humidity):
+        self._humidity = new_humidity
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        self._name = new_name
+
+    @property
+    def room(self):
+        return self._room.name
+
+    @room.setter
+    def room(self, new_room):
+        self._room = new_room
+
+    def temperature_safe(self):
+        if self._critical_temperature > self._temperature:
+            return True
+        else:
+            return False
 
     def __str__(self):
-        return self.name + ' in ' + self.room.name + ' has value' + str(self.value)
+        return self._name + ' in ' + self._room.name + ' has value' + str(self._temperature)
 
 
-# TODO list of devices to be done
+# TODO test profile !!!
 
 
-class DeviceList(models.Model):
-    device = models.OneToOneField(Device, on_delete=models.CASCADE)
-    from_to_time = models.OneToOneField(Timer, on_delete=models.CASCADE)
+class Profile(models.Model):
+    _user = models.OneToOneField(User, on_delete=models.CASCADE)
+    _device = models.ManyToManyField(Device)
+
+    @property
+    def user(self):
+        return self._user
+
+    @user.setter
+    def user(self, new_user):
+        self._user = new_user
+
+    @property
+    def device(self):
+        return self._device
+
+    @device.setter
+    def device(self, new_dev):
+        self._device = new_dev
 
     def __str__(self):
-        return self.device.name + 'is scheduled ' + self.from_to_time.get_time()
-
-
-# TODO test sensor list
-
-class SensorList(models.Model):
-    alarm_value = models.IntegerField(default=50)
-    sensor = models.OneToOneField(Sensor, on_delete=models.CASCADE)
+        return ' '.join((self._user.username, self._user.first_name, self._user.last_name))
--- AutoHome/tests.py
+++ AutoHome/tests.py
@@ -8,13 +8,48 @@ from datetime import timedelta
 from .models import *
 
 
+class TimerModelTest(TestCase):
+    timer = Timer(from_time=timezone.now(), to_time=timezone.now() + timedelta(1))
+
+    def test_timer_calculate_time(self):
+        print(self.timer.calculate_time())
+
+    def test_timer_set_time(self):
+        self.timer.to_time = timezone.now() + timedelta(1)
+        self.timer.from_time = timezone.now()
+
+    def test_timer_get_time(self):
+        print(self.timer)
+        self.test_timer_set_time()
+        print(str(self.timer) + ' - modified')
+
+    def test_zoutro(self):
+        print('=== TESTING TIMER MODEL ENDED ===\n')
+
+
+class RoomModelTest(TestCase):
+    room = Room(name='Podrum')
+
+    def test_aintro(self):
+        print('=== TESTING ROOM MODEL STARTED ===')
+
+    def test_room_name(self):
+        value = 'Kuhinja'
+        print(self.room.name)
+        self.room.name = value
+        print(self.room.name + ' - modified')
+
+    def test_zoutro(self):
+        print('=== TESTING ROOM MODEL ENDED ===\n')
+
+
 class DeviceModelTest(TestCase):
     timer = Timer(from_time=timezone.now(), to_time=timezone.now() + timedelta(1))
-    room = Room('Kitchen')
-    power = True
     modified = timezone.now() + timedelta(1)
+    room = Room(name='Kitchen')
     name = 'test_device'
-    device = Device(modified=modified, dev_power=power, dev_room=room, dev_timer=timer, dev_name=name)
+    power = True
+    device = Device(modification_time=modified, power=power, room=room, timer=timer, name=name)
 
     def test_aintro(self):
         print('\n=== TESTING DEVICE MODEL STARTED ===')
@@ -26,25 +61,27 @@ class DeviceModelTest(TestCase):
         print(str(self.device.modification_time) + ' - modified')
 
     def test_device_room(self):
-        new_room = 'Basement'
+        new_room = Room(name='Basement')
 
-        print(self.device.room)
+        print(self.device.room.name)
         self.device.room = new_room
-        print(self.device.room + ' - modified')
+        print(self.device.room.name + ' - modified')
 
     def test_device_power(self):
-        new_power = 'False'
+        new_power = False
 
-        print(self.device.power)
+        print(str(self.device.power))
         self.device.power = new_power
-        print(self.device.power + ' - modified')
+        print(str(self.device.power) + ' - modified')
 
     def test_device_timer(self):
-        time_from_to = [timezone.now(), timezone.now() + timedelta(1)]
+        time_from = timezone.now()
+        time_to = timezone.now() + timedelta(1)
 
-        print(self.device.timer)
-        self.device.timer = time_from_to
-        print(self.device.timer + ' - modified')
+        print(self.device.timer.to_time)
+        self.device.timer.from_time = time_from
+        self.device.timer.to_time = time_to
+        print(str(self.device.timer) + ' - modified')
 
     def test_device_name(self):
         name = 'new_test_device'
@@ -57,39 +94,71 @@ class DeviceModelTest(TestCase):
         print('=== TESTING DEVICE MODEL ENDED ===\n')
 
 
-class TimerModelTest(TestCase):
-    timer = Timer(from_time=timezone.now(), to_time=timezone.now() + timedelta(1))
+class SensorModelTest(TestCase):
+    room = Room(name="Attic")
+    name = "test sensor"
+    temperature = 10
+    crit_temp = 20
+    humidity = 15
+    sensor = Sensor(name=name, room=room, temperature=temperature, critical_temp=crit_temp, humidity=humidity)
 
     def test_aintro(self):
-        print('=== TESTING TIMER MODEL STARTED ===')
+        print('=== TESTING SENSOR MODEL STARTED ===')
 
-    def test_timer_calculate_time(self):
-        print(self.timer.calculate_time())
+    def test_get_attributes(self):
+        print(f"Alarm temperature is {self.sensor.critical_temp}, temperature is {self.sensor.temperature}",
+              f"whilst the humidity is {self.sensor.humidity}")
+        print(f"{self.sensor.name} is currently at the {self.sensor.room}")
 
-    def test_timer_set_time(self):
-        self.timer.set_time(timezone.now(), timezone.now() + timedelta(1))
+        if not self.sensor.temperature_safe():
+            print("dangeraux")
+        else:
+            print("safe")
 
-    def test_timer_get_time(self):
-        print(self.timer.get_time())
-        self.test_timer_set_time()
-        print(self.timer.get_time() + ' - modified')
-
-    def test_zoutro(self):
-        print('=== TESTING TIMER MODEL ENDED ===\n')
-
-
-class RoomModelTest(TestCase):
-    name = 'Podrum'
-    room = Room(name)
-
-    def test_aintro(self):
-        print('=== TESTING ROOM MODEL STARTED ===')
-
-    def test_room_name(self):
-        value = 'Kuhinja'
-        print(self.name)
-        self.name = value
-        print(self.name + ' - modified')
+    def test_set_attributes(self):
+        self.sensor.room = Room(name="garage")
+        self.sensor.name = "new test sensor"
+        self.sensor.temperature = 30
+        self.sensor.critical_temp = 25
+        self.sensor.humidity = 50
+        self.test_get_attributes()
 
     def test_zoutro(self):
-        print('=== TESTING ROOM MODEL ENDED ===\n')
+        print('=== TESTING SENSOR MODEL ENDED ===\n')
+
+
+class ProfileModelTest(TestCase):
+    # user = User(username="heheyoy", password="supersecretpassword", first_name='Subject 1',
+    #             last_name='Tester 1', last_login=timezone.now())
+    # user.save()
+
+    user = User.objects.get(pk=1)
+
+    from_time = timezone.datetime(2000, 12, 1, 1, 1, 1, 1)
+    to_time = timezone.datetime(3000, 12, 1, 1, 1, 1, 1)
+    timer = Timer(from_time=from_time, to_time=to_time)
+    timer.save()
+
+    modification_time = timezone.now() + timedelta(1)
+    name = 'test_device, profile'
+    # room = Room(name='room')
+    # room.save()
+    room = Room.objects.get(pk=1)
+
+    # device = Device(timer=timer, room=room, name=name, modification_time=modification_time, power=True)
+    # device.save()
+    device = Device.objects.get(pk=1)
+
+    # profile.save()
+    profile = Profile.objects.get(_user=user)
+    profile._device.add(device)
+
+    def test_profile_info(self):
+        print(' '.join(('User info: ', self.profile.user.first_name, self.profile.user.last_name,
+                        self.profile.user.username, self.profile.user.password)))
+
+        print(' '.join(('Device info', self.device.name)))
+
+    def test_change_user(self):
+        new_user = User(username="poyy", password="notsosecretpassword")
+        self.profile.user = new_user
